8f8a66278f99bd789f5576899b89180514bb2fea
Walee Ahmed <dev@walee.me>
2019-03-25 03:35:13 -0400

    added token generation from program

--- lib/src/tokens.dart
+++ lib/src/tokens.dart
@@ -1,8 +1,7 @@
 import 'dart:collection';
-import 'package:meta/meta.dart';
-import 'package:test/test.dart';
+import 'dart:mirrors';
 
-import 'package:matcher/matcher.dart';
+import 'package:test/test.dart';
 
 class Token {
   Token({this.type, this.value, this.charSize});
@@ -19,13 +18,7 @@ class Token {
           String type, String value, String input, int current) =>
       value == input[current]
           ? Token(type: type, value: value, charSize: 1)
-          : Token(type: "Error", value: null, charSize: 0);
-
-  static Token tokenizeParenOpen(String input, int current) =>
-      tokenizeCharacter("paren", "(", input, current);
-
-  static Token tokenizeParenClose(String input, int current) =>
-      tokenizeCharacter("paren", ")", input, current);
+          : Token(type: "SKIP", value: null, charSize: 0);
 
   static Token tokenizePattern(
       String type, RegExp pattern, String input, int current) {
@@ -41,11 +34,19 @@ class Token {
 
       return Token(type: type, value: value, charSize: consumedChar);
     }
-    return Token(type: "EOF", value: null, charSize: 0);
+    return Token(type: "SKIP", value: null, charSize: 0);
   }
 
-  static Token tokenizeNum(String input, int current) =>
-      tokenizePattern("num", RegExp("[0-9]"), input, current);
+  static Token skipWhiteSpace(String input, int i) =>
+      RegExp(r'\s').hasMatch(input[i])
+          ? Token(type: "blank", value: null, charSize: 1)
+          : Token(type: "SKIP", value: null, charSize: 0);
+
+  static Token tokenizeParenOpen(String input, int current) =>
+      tokenizeCharacter("paren", "(", input, current);
+
+  static Token tokenizeParenClose(String input, int current) =>
+      tokenizeCharacter("paren", ")", input, current);
 
   static Token tokenizeName(String input, int current) =>
       tokenizePattern("name", RegExp("[A-Za-z]"), input, current);
@@ -68,27 +69,9 @@ class Token {
     return Token(type: "EOF", value: null, charSize: 0);
   }
 
-  static Token skipWhiteSpace(String input, int i) =>
-      RegExp(r'\s').hasMatch(input)
-          ? Token(type: "EOF", value: null, charSize: 1)
-          : Token(type: "EOF", value: null, charSize: 0);
+  static Token tokenizeNum(String input, int current) =>
+      tokenizePattern("num", RegExp("[0-9]"), input, current);
 }
-
-class _TokenMatcher extends Matcher {
-  _TokenMatcher({@required this.token});
-  final Token token;
-
-  bool matches(item, Map matchState) {
-    return item is Token && token.equals(item);
-  }
-
-  Description describe(Description description) => description;
-  // .add('checking if two Tokens are equal')
-  // .addDescriptionOf(collapseWhitespace(_prefix))
-  // .add(' ignoring whitespace');
-}
-
-Matcher tokenEquals(m) => _TokenMatcher(token: m);
 
 expectTokens(Token a, Token b) {
   expect(
@@ -109,7 +92,7 @@ class Tokens extends IterableBase {
   String _program;
   set program(i) => _program = i;
 
-  List<Token> _tokens;
+  List<Token> _tokens = [];
   get value => _tokens;
   get iterator => _tokens.iterator;
 
@@ -117,11 +100,76 @@ class Tokens extends IterableBase {
   int _numeralAtomCount = 0;
   int _parenCloseCount = 0;
   int _parenOpenCount = 0;
+  int _numSum = 0;
 
-  void generate() {
-    if (_program == null) throw ("Please initialize program");
-    _tokens = [];
+// TODO: Add test cases for getNextToken
+  static Token getNextToken(String input, int i) {
+    bool tokenized = false;
+    Token token;
+    ClassMirror tm = reflectClass(Token);
+    for (var m in tm.staticMembers.values) {
+      if (m.simpleName != Symbol('tokenizeCharacter') &&
+          m.simpleName != Symbol('tokenizePattern') &&
+          !tokenized) {
+        token = tm.invoke(m.simpleName, [input, i]).reflectee;
+
+        if (token.type != "SKIP" && token.charSize > 0) {
+          tokenized = true;
+        }
+      }
+      if (tokenized) break;
+    }
+    return token;
   }
 
-  Token getNextToken() {}
+  void keepCount(Token token) {
+    switch (token.type) {
+      case "name":
+        _literalAtomCount += 1;
+        break;
+      case "num":
+        _numeralAtomCount += 1;
+        _numSum += num.tryParse(token.value);
+        break;
+      case "paren":
+        if (token.value == "(")
+          _parenOpenCount += 1;
+        else
+          _parenCloseCount += 1;
+        break;
+      default:
+        break;
+    }
+  }
+
+// TODO: Add more generaic test cases
+  void generate() {
+    int i = 0;
+    _program = _program.trim();
+
+    while (i < _program.length) {
+      Token token = getNextToken(_program, i);
+      i += token.charSize;
+      if (token.type != "blank") _tokens.add(token);
+
+      keepCount(token);
+    }
+    if (_program.length < 1) {
+      _tokens.add(Token(type: "EOF", value: null));
+    }
+  }
+
+  void prettyPrint() {
+    /** 
+     * LITERAL ATOMS: 5, DEFUN, F23, X, PLUS, X
+     * NUMERIC ATOMS: 2, 67
+     * OPEN PARENTHESES: 3
+     * CLOSING PARENTHESES: 3
+    */
+    print(
+        'LITERAL ATOMS: ${this._literalAtomCount}, ${this._tokens.where((token) => token.type == "name").map((token) => token.value).join(" , ")}');
+    print('NUMERIC ATOMS: ${this._numeralAtomCount}, ${this._numSum}');
+    print('OPEN PARENTHESES: ${this._parenOpenCount}');
+    print('CLOSE PARENTHESES: ${this._parenCloseCount}');
+  }
 }
