addc0010c62cf5d688845b3e78c95104c4ec7677
Walee Ahmed <dev@walee.me>
2019-03-25 03:35:41 -0400

    added basic entry program

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,11 @@
+import 'package:darty_lisp/darty_lisp.dart';
+
 main(List<String> args) {
   print(args);
+
+  Tokens tokens = Tokens();
+  tokens.program = '(add 2 3)';
+
+  tokens.generate();
+  tokens.prettyPrint();
 }
